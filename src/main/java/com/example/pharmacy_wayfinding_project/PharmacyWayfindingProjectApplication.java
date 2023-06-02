53e573c8f93864c8a37afc1f03d87d503a556fa5
bokyoung shin <sbk8689@gmail.com>
2023-06-02 11:51:35 +0900

    first commit

---
+++ src/main/java/com/example/pharmacy_wayfinding_project/PharmacyWayfindingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.pharmacy_wayfinding_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyWayfindingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmacyWayfindingProjectApplication.class, args);
+    }
+
+}
