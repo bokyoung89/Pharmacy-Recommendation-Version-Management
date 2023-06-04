86577c908212bd451db90e8569fc36f722874fe7
bokyoung shin <sbk8689@gmail.com>
2023-06-04 13:42:35 +0900

    #6 - test : Testcontainers를 이용한 통합 테스트 환경 구성

---
+++ src/main/java/com/example/pharmacy_wayfinding_project/pharmacy/repository/PharmacyRepository.java
@@ -0,0 +1,8 @@
+package com.example.pharmacy_wayfinding_project.pharmacy.repository;
+
+import com.example.pharmacy_wayfinding_project.pharmacy.entity.Pharmacy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PharmacyRepository extends JpaRepository<Pharmacy, Long> {
+
+}
