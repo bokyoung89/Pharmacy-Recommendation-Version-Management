1deb1ffa2096fa9fa507cfe84a34348afc661ce6
bokyoung shin <sbk8689@gmail.com>
2023-06-05 19:50:46 +0900

    #7 - test : Kakao 주소검색 api 테스트 케이스 작성

--- src/test/groovy/com/example/pharmacy_wayfinding_project/pharmacy/repository/PharmacyRepositoryTest.groovy
+++ src/test/groovy/com/example/pharmacy_wayfinding_project/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -7,7 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired
 class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest{
 
     @Autowired
-    private PharmacyRepository pharmacyRepository;
+    private PharmacyRepository pharmacyRepository
+
+    def setup() {
+        pharmacyRepository.deleteAll()
+    }
 
     def "PharmacyRepository save"() {
         given:
@@ -31,6 +35,28 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest{
         result.getPharmacyName() == name
         result.getLatitude() == latitude
         result.getLongitude() == longitude
+    }
+
+    def "PharmacyRepository saveAll"() {
+        given:
+        String address = "서울 특별시 성북구 종암동"
+        String name = "문래 약국"
+        double latitude = 36.11
+        double longitude = 128.11
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build()
+
+        when:
+        pharmacyRepository.saveAll(Arrays.asList(pharmacy))
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.size() == 1
     }
 
 }
