6800c827c3021a7728d6de2f19ed693e5782baba
bokyoung shin <sbk8689@gmail.com>
2023-06-11 11:11:16 +0900

    #19 - feat : 메인화면, 결과화면 구현

--- src/main/java/com/example/pharmacy_wayfinding_project/pharmacy/service/PharmacyRecommendationService.java
+++ src/main/java/com/example/pharmacy_wayfinding_project/pharmacy/service/PharmacyRecommendationService.java
@@ -3,6 +3,7 @@ package com.example.pharmacy_wayfinding_project.pharmacy.service;
 import com.example.pharmacy_wayfinding_project.api.dto.DocumentDto;
 import com.example.pharmacy_wayfinding_project.api.dto.KakaoApiResponseDto;
 import com.example.pharmacy_wayfinding_project.api.service.KakaoAddressSearchService;
+import com.example.pharmacy_wayfinding_project.direction.dto.OutputDto;
 import com.example.pharmacy_wayfinding_project.direction.entity.Direction;
 import com.example.pharmacy_wayfinding_project.direction.service.DirectionService;
 import lombok.RequiredArgsConstructor;
@@ -10,8 +11,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -21,20 +24,32 @@ public class PharmacyRecommendationService {
     private final KakaoAddressSearchService kakaoAddressSearchService;
     private final DirectionService directionService;
 
-    public void recommendPharmacyList(String address) { //문자열 기반 주소로 고객이 요청
+    public List<OutputDto> recommendPharmacyList(String address) { //문자열 기반 주소로 고객이 요청
         //위치기반 데이터로 변환
         KakaoApiResponseDto kakaoApiResponseDto = kakaoAddressSearchService.requestAddressSearch(address);
         //응답값에 대해 validation check
         if(Objects.isNull(kakaoApiResponseDto) || CollectionUtils.isEmpty(kakaoApiResponseDto.getDocumentList())) {
             log.error("[PharmacyRecommendationService recommendPharmacyList fail] Input address: {}", address);
-            return;
+            return Collections.emptyList();
         }
         //변환된 위도, 경도 기준으로 가까운 약국을 찾는다.
         DocumentDto documentDto = kakaoApiResponseDto.getDocumentList().get(0);
         //거리 계산 알고리즘을 통해 가까운 약국 리스트를 리턴한다.
         List<Direction> directionList = directionService.buildDirectionList(documentDto);
         //고객에서 길안내를 한다.
-        directionService.saveAll(directionList);
+        return directionService.saveAll(directionList)
+                .stream()
+                .map(this::convertToOutputDto)
+                .collect(Collectors.toList());
+    }
 
+    private OutputDto convertToOutputDto(Direction direction) {
+        return OutputDto.builder()
+                .pharmacyName(direction.getTargetPharmacyName())
+                .pharmacyAddress(direction.getInputAddress())
+                .directionUrl("todo")   // todo
+                .roadViewUrl("todo")
+                .distance(String.format("%.2f km", direction.getDistance()))
+                .build();
     }
 }
