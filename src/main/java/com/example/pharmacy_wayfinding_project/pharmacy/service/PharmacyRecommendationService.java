d008d0f9756d98b9478471245294a188e58e5bb8
bokyoung shin <sbk8689@gmail.com>
2023-06-13 19:20:34 +0900

    fix : OutputDto 생성 시 약국 주소가 고객이 입력한 주소로 잘못 셋팅돼있어 수정함

--- src/main/java/com/example/pharmacy_wayfinding_project/pharmacy/service/PharmacyRecommendationService.java
+++ src/main/java/com/example/pharmacy_wayfinding_project/pharmacy/service/PharmacyRecommendationService.java
@@ -57,7 +57,7 @@ public class PharmacyRecommendationService {
 
         return OutputDto.builder()
                 .pharmacyName(direction.getTargetPharmacyName())
-                .pharmacyAddress(direction.getInputAddress())
+                .pharmacyAddress(direction.getTargetAddress())
                 .directionUrl(baseUrl + base62Service.encodeDirection(direction.getId()))
                 .roadViewUrl(ROAD_VIEW_BASE_URL + direction.getTargetLatitude() + "," + direction.getTargetLongitude())
                 .distance(String.format("%.2f km", direction.getDistance()))
