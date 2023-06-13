eec45cc7e77e04a7e3f0a750d73daa202b1c1440
bokyoung shin <sbk8689@gmail.com>
2023-06-13 10:24:50 +0900

    #25 - feat : redis를 이용한 성능 개선

--- src/main/java/com/example/pharmacy_wayfinding_project/pharmacy/service/PharmacySearchService.java
+++ src/main/java/com/example/pharmacy_wayfinding_project/pharmacy/service/PharmacySearchService.java
@@ -1,5 +1,6 @@
 package com.example.pharmacy_wayfinding_project.pharmacy.service;
 
+import com.example.pharmacy_wayfinding_project.pharmacy.cache.PharmacyRedisTemplateService;
 import com.example.pharmacy_wayfinding_project.pharmacy.dto.PharmacyDto;
 import com.example.pharmacy_wayfinding_project.pharmacy.entity.Pharmacy;
 import lombok.RequiredArgsConstructor;
@@ -15,10 +16,13 @@ import java.util.stream.Collectors;
 public class PharmacySearchService {
 
     private final PharmacyRepositoryService pharmacyRepositoryService;
+    private final PharmacyRedisTemplateService pharmacyRedisTemplateService;
 
     public List<PharmacyDto> searchPharmacyDtoList() {
 
         //redis
+        List<PharmacyDto> pharmacyDtoList = pharmacyRedisTemplateService.findAll();
+        if(!pharmacyDtoList.isEmpty()) return pharmacyDtoList;
 
         //db
         return pharmacyRepositoryService.findAll()
